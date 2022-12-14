126e398d7c3d99ca1bf3d57d287c4fedc2366703
Noemi Deppenwiese <noemi.deppenwiese@uk-erlangen.de>
2022-12-14 22:26:57 +0000

    Day 13 complete

--- day13.rb
+++ day13.rb
@@ -1,5 +1,15 @@
 require 'yaml'
 
+class Package
+    attr_accessor :content
+    def initialize(content)
+        @content = content
+    end
+
+    def to_s
+        return content.inspect
+    end
+end
 
 def compare_ints(left,right)
     if right < left
@@ -32,9 +42,7 @@ def compare_lists(left,right)
 
             elsif right[index].is_a? Array
 
-                left = [left[index]]
-                right = right[index]
-                correct = compare_lists(left,right)
+                correct = compare_lists([left[index]],right[index])
 
             end
 
@@ -42,9 +50,7 @@ def compare_lists(left,right)
 
             if(right[index].is_a? Integer)
 
-                left = left[index]
-                right = [right[index]]
-                correct = compare_lists(left,right)
+                correct = compare_lists(left[index],[right[index]])
 
             elsif right[index].is_a? Array
                 correct = compare_lists(left[index],right[index])
@@ -57,6 +63,7 @@ end
 
 input = File.readlines('input.txt')
 
+=begin part 1
 counter = 0
 correct_indices =[]
 while(input.length > counter)
@@ -71,4 +78,40 @@ while(input.length > counter)
     counter = counter + 3
 end
 
-puts correct_indices.sum
+puts correct_indices.sum 
+=end
+
+def quicksort(input_list)
+    if(input_list.length == 1 || input_list.length == 0)
+        return input_list
+    end
+    pivot = rand(0..(input_list.length-1))
+    p_item = input_list[pivot]
+    input_list.delete(p_item)
+    small_list = []
+    large_list = []
+    input_list.each do |element|
+        if compare_lists(p_item.content,element.content)
+            large_list.append(element)
+        else
+            small_list.append(element)
+        end
+    end
+    small_list = quicksort(small_list)
+    large_list = quicksort(large_list)
+    return small_list.append(p_item).concat(large_list)
+end
+
+div1 = Package.new([[2]])
+div2 = Package.new([[6]])
+all_pkgs = []
+all_pkgs.append(div1)
+all_pkgs.append(div2)
+input.each do |line|
+    if !line.strip.empty?
+        all_pkgs.append(Package.new(YAML.load(line)))
+    end
+end
+all_pkgs = quicksort(all_pkgs)
+puts (all_pkgs.find_index(div1)+1)*(all_pkgs.find_index(div2)+1)
+
